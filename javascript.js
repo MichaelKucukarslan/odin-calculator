0cd369f7e68e34ad651ba15938a6123f7d179683
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-07-01 17:31:34 -0700

    Added functionality to operators. Still has a few bugs

--- javascript.js
+++ javascript.js
@@ -1,133 +1,139 @@
 const display = document.getElementById("display");
+const numbers = Array.from(document.getElementsByClassName("numbers"));
+const operators = Array.from(document.getElementsByClassName("operator"));
+
+display.value = "";
+let number1 = 0;
+let number2 = 0;
+let number_total = "";
+
+numbers.forEach((element) =>
+    element.addEventListener('click', () => appendNumber(element.textContent))
+);
+
+operators.forEach(element =>
+    element.addEventListener("click", () => setOperator(element.textContent))
+);
+
+// user presses keys to get a number
+// user presses an operation
+// save that number
+// clear the screen to get 2nd number
+
+// > user presses keys to get another number
+
+// > user presses operation
+// do the operation
+// show the user the total
+// save the total as number1, clear number2
+
+// > users presses total key
+// do the operation
+// show the users the total
+// save the total as number1, clear number2
+
+// > user presses another operation to do a different operation
+// change the operation without loosing number1
+
+let savedOperation = "";
+function setOperator(operation) {
+    // Save the number
+    savedOperation = operation;
+    if (number1 === 0) {
+        number1 = display.value;
+        display.value = 0;
+    } else if (number2 === 0 && display.value !== 0) {
+        number2 = display.value;
+    }
+    console.log(number1 + " " + savedOperation + " " + number2)
+    // do the operation if there are 2 numbers
+    if (number1 !== 0 && number2 !== 0) {
+        calculate(operation, number1, number2);
+    }
+    // display the operation
+}
+
+function calculate(operation, num1, num2) {
+    if (num1 == 0 || num2 == 0) return;
+    switch (operation) {
+        case 'X':
+            display.value = num1 * num2;
+            break;
+        case '-':
+            display.value = num1 - num2;
+            break;
+        case '+':
+            display.value = parseInt(num1) + parseInt(num2);
+            break;
+        case '%':
+            display.value /= 100;
+            break;
+        case '/':
+            if (num2 == 0){
+                alert("Can not divide by 0");
+                clear();
+                display.value = 0;
+                return;
+            }
+            display.value = num1 / num2;
+            break;
+        default:
+            break;
+    }
+    clear();
+}
 
 const button_clear = document.getElementById("clear");
-button_clear.addEventListener("click", clear);
+button_clear.addEventListener("click", clearButton);
+function clearButton(){
+    display.value = 0;
+    clear();
+}
+
 function clear() {
-    display.value = "";
+    number1 = 0;
+    number2 = 0;
+    decimal_used = false;
 }
 
 const button_delete = document.getElementById("delete");
 button_delete.addEventListener("click", delete_func);
-function delete_func(){
+function delete_func() {
     display.value = display.value.substr(0, display.value.length - 1);
 }
 
 const button_on = document.getElementById("on");
 button_on.addEventListener("click", delete_on);
-function delete_on(){
+function delete_on() {
+    display.value = 0;
     console.log("on");
 }
 
-const button_multiply = document.getElementById("multiply");
-button_multiply.addEventListener("click", button_multiply_func);
-function button_multiply_func(){
-    console.log("multiply");
-}
-
-const button_percent = document.getElementById("percent");
-button_percent.addEventListener("click", button_percent_func);
-function button_percent_func(){
-    console.log("percent");
-}
-
-const button_subtract = document.getElementById("subtract");
-button_subtract.addEventListener("click", button_subtract_func);
-function button_subtract_func(){
-    console.log("subtract");
-}
-
-const button_divide = document.getElementById("divide");
-button_divide.addEventListener("click", button_divide_func);
-function button_divide_func(){
-    console.log("divide");
-}
-
-const button_add = document.getElementById("add");
-button_add.addEventListener("click", button_add_func);
-function button_add_func(){
-    console.log("add");
-}
-
+// Handle total button
 const button_total = document.getElementById("total");
 button_total.addEventListener("click", button_total_func);
-function button_total_func(){
-    console.log("total");
+function button_total_func() {
+    if (!number2){
+        number2 = display.value;
+    }
+    calculate(savedOperation, number1, number2);
 }
 
+// Handle decimal button
 let decimal_used = false;
 const button_decimal = document.getElementById("decimal");
 button_decimal.addEventListener("click", button_decimal_func);
-function button_decimal_func(){
-    if (!decimal_used){
+function button_decimal_func() {
+    if (!decimal_used) {
         display.value += "."
     }
     decimal_used = true;
 }
 
-const button_0 = document.getElementById("0");
-button_0.addEventListener("click", button_0_func);
-function button_0_func(){
-    display.value += 0;
-}
-
-const button_00 = document.getElementById("00");
-button_00.addEventListener("click", button_00_func);
-function button_00_func(){
-    display.value += 0;
-    display.value += 0;
-}
-
-const button_1 = document.getElementById("1");
-button_1.addEventListener("click", button_1_func);
-function button_1_func(){
-    display.value += 1;
-}
-
-const button_2 = document.getElementById("2");
-button_2.addEventListener("click", button_2_func);
-function button_2_func(){
-    display.value += 2;
-}
-
-const button_3 = document.getElementById("3");
-button_3.addEventListener("click", button_3_func);
-function button_3_func(){
-    display.value += 3;
-}
-
-const button_4 = document.getElementById("4");
-button_4.addEventListener("click", button_4_func);
-function button_4_func(){
-    display.value += 4;
-}
-
-const button_5 = document.getElementById("5");
-button_5.addEventListener("click", button_5_func);
-function button_5_func(){
-    display.value += 5;
-}
-
-const button_6 = document.getElementById("6");
-button_6.addEventListener("click", button_6_func);
-function button_6_func(){
-    display.value += 6;
-}
-
-const button_7 = document.getElementById("7");
-button_7.addEventListener("click", button_7_func);
-function button_7_func(){
-    display.value += 7;
-}
-
-const button_8 = document.getElementById("8");
-button_8.addEventListener("click", button_8_func);
-function button_8_func(){
-    display.value += 8;
-
-}
-
-const button_9 = document.getElementById("9");
-button_9.addEventListener("click", button_9_func);
-function button_9_func(){
-    display.value += 9;
+function appendNumber(number) {
+    if (display.value === '0') {
+        display.value = number;
+    } else {
+        display.value += number;
+    }
 }
