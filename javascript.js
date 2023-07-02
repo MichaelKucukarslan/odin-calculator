620736fb5b7041c4dde8c4ffb464a6e23c4f3f47
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-07-02 13:21:37 -0700

    Getting calculator to work with number operand number
    Got the calculator to work with number operand number
    number > operand > number ? equals (shows total)
    number > operand > number > operand (shows total) > number > equals (shows total)

--- javascript.js
+++ javascript.js
@@ -3,9 +3,11 @@ const numbers = Array.from(document.getElementsByClassName("numbers"));
 const operators = Array.from(document.getElementsByClassName("operator"));
 
 display.value = "";
-let number1 = 0;
-let number2 = 0;
-let number_total = "";
+let number1 = null;
+let number2 = null;
+let number_total = null;
+let clearScreen = false;
+let savedOperation = null;
 
 numbers.forEach((element) =>
     element.addEventListener('click', () => appendNumber(element.textContent))
@@ -14,11 +16,6 @@ numbers.forEach((element) =>
 operators.forEach(element =>
     element.addEventListener("click", () => setOperator(element.textContent))
 );
-
-// user presses keys to get a number
-// user presses an operation
-// save that number
-// clear the screen to get 2nd number
 
 // > user presses keys to get another number
 
@@ -35,26 +32,20 @@ operators.forEach(element =>
 // > user presses another operation to do a different operation
 // change the operation without loosing number1
 
-let savedOperation = "";
 function setOperator(operation) {
     // Save the number
     savedOperation = operation;
-    if (number1 === 0) {
+    if (number1 === null) {
         number1 = display.value;
-        display.value = 0;
-    } else if (number2 === 0 && display.value !== 0) {
+        clearScreen = true;
+    } else if (number2 === null && !clearScreen) {
         number2 = display.value;
-    }
-    console.log(number1 + " " + savedOperation + " " + number2)
-    // do the operation if there are 2 numbers
-    if (number1 !== 0 && number2 !== 0) {
         calculate(operation, number1, number2);
     }
-    // display the operation
+    console.log(number1 + " " + savedOperation + " " + number2)
 }
 
 function calculate(operation, num1, num2) {
-    if (num1 == 0 || num2 == 0) return;
     switch (operation) {
         case 'X':
             display.value = num1 * num2;
@@ -63,13 +54,13 @@ function calculate(operation, num1, num2) {
             display.value = num1 - num2;
             break;
         case '+':
-            display.value = parseInt(num1) + parseInt(num2);
+            display.value = parseInt(num1) + parseInt(num2); // Need to get decimals
             break;
         case '%':
-            display.value /= 100;
+            display.value = display.value / 100;
             break;
         case '/':
-            if (num2 == 0){
+            if (num2 == 0) {
                 alert("Can not divide by 0");
                 clear();
                 display.value = 0;
@@ -80,43 +71,28 @@ function calculate(operation, num1, num2) {
         default:
             break;
     }
-    clear();
+    console.log(number1 + " " + savedOperation + " " + number2 + " ClearScreen? " + clearScreen + " Operand: " + operation);
+    continueNewCalculation();
+    console.log(number1 + " " + savedOperation + " " + number2 + " ClearScreen? " + clearScreen + " Operand: " + operation);
 }
 
-const button_clear = document.getElementById("clear");
-button_clear.addEventListener("click", clearButton);
-function clearButton(){
-    display.value = 0;
-    clear();
-}
-
-function clear() {
-    number1 = 0;
-    number2 = 0;
-    decimal_used = false;
-}
-
-const button_delete = document.getElementById("delete");
-button_delete.addEventListener("click", delete_func);
-function delete_func() {
-    display.value = display.value.substr(0, display.value.length - 1);
-}
-
-const button_on = document.getElementById("on");
-button_on.addEventListener("click", delete_on);
-function delete_on() {
-    display.value = 0;
-    console.log("on");
+function continueNewCalculation() {
+    savedOperation = null;
+    clearScreen = true;
+    number1 = display.value;
+    number2 = null;
 }
 
 // Handle total button
 const button_total = document.getElementById("total");
 button_total.addEventListener("click", button_total_func);
 function button_total_func() {
-    if (!number2){
+    if (number1 != null && number2 == null){
         number2 = display.value;
+        calculate(savedOperation, number1, number2);
+        return;
     }
-    calculate(savedOperation, number1, number2);
+    continueNewCalculation();
 }
 
 // Handle decimal button
@@ -131,9 +107,43 @@ function button_decimal_func() {
 }
 
 function appendNumber(number) {
-    if (display.value === '0') {
+    if (!savedOperation && clearScreen){
+        number1 = null;
+    }
+    if (clearScreen) {
+        display.value = number;
+    } else if (display.value === '0') {
         display.value = number;
     } else {
         display.value += number;
     }
+    clearScreen = false;
+}
+
+const button_delete = document.getElementById("delete");
+button_delete.addEventListener("click", delete_func);
+function delete_func() {
+    display.value = display.value.substr(0, display.value.length - 1);
+}
+
+const button_on = document.getElementById("on");
+button_on.addEventListener("click", delete_on);
+function delete_on() {
+    display.value = 0;
+}
+
+const button_clear = document.getElementById("clear");
+button_clear.addEventListener("click", clearButton);
+function clearButton() {
+    display.value = 0;
+    clear();
+}
+
+function clear() {
+    number1 = null;
+    number2 = null;
+    number_total = null;
+    decimal_used = false;
+    savedOperation = null;
+    clearScreen = true;
 }
